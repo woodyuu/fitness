75a8b1e62883f86ecb172bf1113f28a4a4155aff
woody <onlyhale@naver.com>
2023-11-24 10:21:25 +0900

    라우터 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './asset/css/App.css';
 import Main_Page from './Components/Main_Page/Main_Page';
 import Common_Contents from './Components/Common_Contents/Common_Contents'
 import Sub_Page from './Components/Sub_Page/Sub_Page'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import Login from './Components/Sub_Page/Login/Login';
 import Map from './Components/Common_Contents/Map/Map'
 import Condition from './Components/Sub_Page/Login/Condition';
@@ -14,7 +14,9 @@ import Condition from './Components/Sub_Page/Login/Condition';
 
 function App() {
   return (    
-    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+    <HashRouter  
+    // basename={process.env.PUBLIC_URL}
+    >
       <Routes>
         <Route path="/" element={<Main_Page />} />
         <Route path="sub" element={<Sub_Page />} />
@@ -24,7 +26,7 @@ function App() {
         <Route path="map" element={<Map />}/>
 
       </Routes>
-    </BrowserRouter>    
+    </HashRouter>    
   )
 }
 
